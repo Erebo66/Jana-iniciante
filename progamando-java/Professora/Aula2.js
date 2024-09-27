57ea21a38da743bb24368391d50fba564a70e532
Erebo66 <caetanocarlos894@gmail.com>
2024-09-27 14:54:31 -0300

    Create Aula2.js

---
+++ progamando-java/Professora/Aula2.js
@@ -0,0 +1,88 @@
+//Operadores
+
+//const x = 4;
+//const y = 5
+
+//const soma = x + y
+//const subtracao = x - y
+//const multiplicacao = x * y
+//const divisao = x / y;
+
+//console.log(soma);
+//console.log(subtracao);
+//console.log(multiplicacao);
+//console.log(divisao);
+
+//===================================
+
+//console.log(3 % 2)
+//console.log(4 % 2)
+//console.log(5 % 2)
+//console.log(6 % 2)
+//console.log(7 % 2)
+//console.log(8 % 2)
+//console.log(9 % 2)
+//console.log(10 % 2)
+
+//=======================
+//const z = 1;
+
+//z = z + 1;
+
+//console.log(z)
+
+//===================
+
+//let z = 1;
+
+//z = z + 1;
+
+//console.log(z)
+
+//=======================
+
+/*let z = 100;
+
+z ++;
+
+console.log(z)*/
+
+//Operadores de comparação 
+
+const a = 1;
+const b = 2;
+
+console.log(a < b)
+console.log(a > b)
+console.log(a <= b)
+console.log(a >= b)
+console.log(a == b)
+console.log(a != b)
+
+// Operadores Lógicos
+
+// && -> E
+// || -> OU
+// ! Não (Reveter o valor)
+
+true && true; true
+true && false; false
+false && true; false
+false && false; false
+
+console.log(a < b && a > b)
+true && false
+console.log(a < b && a != b)
+true && true
+console.log(a == b && a > b)
+false && false
+
+console.log(a < b || 1 > 2);
+console.log(a == b || a != b);
+console.log(a == b || a > b);
+
+console.log(!true)
+console.log(!(a == b));
+
+
+
